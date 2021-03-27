2edad84d935affa023b15d530fc5e2004b5f266f
Neil Bowers <neil@bowers.com>
2021-03-27 11:43:14 +0000

    Refactored the doc and added SEE ALSO links to other hex dumpers

--- lib/Data/HexDump.pm
+++ lib/Data/HexDump.pm
@@ -12,6 +12,7 @@
 
 package Data::HexDump;
 
+use 5.006;
 use strict;
 use warnings;
 
@@ -93,24 +94,24 @@ sub dump {
       $out .= sprintf "%02X", ord $v;
       $out .= " " if $i < 15;
       $out .= "- " if $i == 7 &&
-	(length $val ||	!($self->{'eod'} || length $val));
+                      (length $val || !($self->{'eod'} || length $val));
       $i++;
       $l .= ord($v) >= 0x20 && ord($v) <= 0x7E ? $v : ".";
       if ($i == 16) {
-	$i = 0;
-	$j++;
-	$out .= "  " . $l;
-	$l = "";
-	$out .= "\n";
-	if (defined $self->{'blocksize'} && $self->{'blocksize'} &&
-	    ($j - $self->{'j'}) > $self->{'blocksize'} / 16) {
-	  $self->{'i'} = $i;
-	  $self->{'j'} = $j;
-	  $self->{'val'} = $val;
-	  return $out;
-	}
-	$out .= sprintf "%08X  ", $j * 16 if length $val || !length $val &&
-	  !$self->{'eod'};
+        $i = 0;
+        $j++;
+        $out .= "  " . $l;
+        $l = "";
+        $out .= "\n";
+        if (defined $self->{'blocksize'} && $self->{'blocksize'} &&
+                    ($j - $self->{'j'}) > $self->{'blocksize'} / 16) {
+          $self->{'i'} = $i;
+          $self->{'j'} = $j;
+          $self->{'val'} = $val;
+          return $out;
+        }
+        $out .= sprintf "%08X  ", $j * 16 if length $val || !length $val &&
+                                             !$self->{'eod'};
       }
     }
   }
@@ -176,38 +177,59 @@ Data::HexDump - Hexadecial Dumper
   use Data::HexDump;
 
   my $buf = "foo\0bar";
-  print HexDump $buf;
+  print HexDump($buf);
 
-  or
+which produces:
 
-  my $f = new Data::HexDump;
-  $f->data($buf);
-  print $f->dump;
+           00 01 02 03 04 05 06 07 - 08 09 0A 0B 0C 0D 0E 0F  0123456789ABCDEF
 
-  or
+ 00000000  23 21 2F 75 73 72 2F 62 - 69 6E 2F 70 65 72 6C 0A  #!/usr/bin/perl.
+ 00000010  75 73 65 20 73 74 72 69 - 63 74 3B 0A 75 73 65 20  use strict;.use
+ 00000020  77 61 72 6E 69 6E 67 73 - 3B 0A 0A 70 72 69 6E 74  warnings;..print
+ 00000030  20 22 48 65 6C 6C 6F 2C - 20 77 6F 72 6C 64 5C 6E   "Hello, world\n
+ 00000040  22 3B 0A                                           ";.
 
-  my $fh = new FileHandle $file2dump;
-  my $f = new Data::HexDump;
-  $f->fh($fh);
-  $f->block_size(1024);
-  print while $_ = $f->dump;
-  close $fh;
-
-  or
-
-  my $f = new Data::HexDump;
-  $f->file($file2dump);
-  $f->block_size(1024);
-  print while $_ = $f->dump;
 
 =head1 DESCRIPTION
 
-Dump in hexadecimal the content of a scalar. The result is returned in a
-string. Each line of the result consists of the offset in the
-source in the leftmost column of each line, followed by one or more
-columns of data from the source in hexadecimal. The rightmost column
-of each line shows the printable characters (all others are shown
-as single dots).
+This module will generate a hexadecimal dump of a data string or file.
+You can either use the exported function,
+as shown in the SYNOPSIS above,
+or the OO interface, described below.
+
+The result is returned in a string.
+Each line of the result consists of the offset in the
+source in the leftmost column of each line,
+followed by one or more columns of data from the source in hexadecimal.
+The rightmost column of each line shows the printable characters
+(all others are shown as single dots).
+
+=head2 Functional Interface
+
+This module exports a single function, C<HexDump>,
+which takes a scalar value and returns a string which
+contains the hexdump of the passed data.
+
+
+=head2 OO Interface
+
+You first construct a C<Data::HexDump> object,
+then tell it where to get the data from,
+and then generate the hex dump:
+
+    my $dh = Data::HexDump->new();
+
+    $dh->data($scalar);      # dump the data in this scalar
+    $dh->fh($fh);            # read this filehandle
+    $dh->file($filename);    # read this file and dump contents
+
+    print while $_ = $f->dump;
+
+The different potential sources for data are considered
+in the order given above,
+so if you pass to the C<data> method,
+then any subsequent calls to C<fh()> or C<file()>
+will have no effect.
 
 =head1 COPYRIGHT
 
@@ -219,12 +241,31 @@ modify it under the same terms as Perl itself.
 
 Fabien Tassin E<lt>fta@oleane.netE<gt>
 
-=head1 VERSION
-
-0.02 - Second release (September 1999)
-
 =head1 SEE ALSO
 
-perl(1)
+L<Data::Hexify>, by Johan Vromans, is another simple option,
+similar to this module. Last release in 2004.
+
+L<Data::Hexdumper> (by David Cantrell, DCANTRELL)
+is another hex dumper,
+with more features than this module.
+
+L<App::colourhexdump> (by Kent Fredric, RIP)
+provides a script which gives colourised output
+with character class highlighting.
+
+L<Data::HexDump::Range> provides more functions, colour output,
+and the ability to skip uninteresting parts of the input data.
+
+L<Data::HexDump::XXD> provides hex dumps like xxd.
+It doesn't say what xxd is, or provide a link,
+and there's no example output.
+But if you know and like xxd, this might be the one for you!
+
+L<Devel::Hexdump> provides some configuration options,
+but there are other more featured modules,
+and this one doesn't have example output in the doc.
+
+L<String::HexConvert> will convert ASCII strings to hex and reverse.
 
 =cut
